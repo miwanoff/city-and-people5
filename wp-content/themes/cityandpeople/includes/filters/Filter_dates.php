a64cd657f1e9adbbbbb1dfb9baa6491e59b0f51c
arhibober <alex_lvovich_iff@mail.ru>
2022-04-09 13:30:09 +0300

    Revert "Experement bad"
    This reverts commit f75ba5b6014810ff67eb75204304aece62551e6f.

--- wp-content/themes/cityandpeople/includes/filters/Filter_dates.php
+++ wp-content/themes/cityandpeople/includes/filters/Filter_dates.php
@@ -1,63 +1,56 @@
 <?php
-class Filter_dates
-{
-	public function __construct()
-	{
-	}
-	public function my_filter_function()
-	{
-		//echo "jjjj";
+
+function my_filter_function(){
+	//echo "jjjj";
 	 
-		//echo " pdn: ".$_POST ["new_date"]." pdo: ".$_POST ["old_date"];
-		// create $args['meta_query'] array if one of the following fields is filled
-		if( isset( $_POST['new_date'] ) && $_POST['new_date'] || isset( $_POST['old_date'] ) && $_POST['old_date'])
-			$args['meta_query'] = array( 'relation'=>'AND' ); // AND means that all conditions of meta_query should be true
+	//echo " pdn: ".$_POST ["new_date"]." pdo: ".$_POST ["old_date"];
+	// create $args['meta_query'] array if one of the following fields is filled
+	if( isset( $_POST['new_date'] ) && $_POST['new_date'] || isset( $_POST['old_date'] ) && $_POST['old_date'])
+		$args['meta_query'] = array( 'relation'=>'AND' ); // AND means that all conditions of meta_query should be true
  
-		// if both minimum price and maximum price are specified we will use BETWEEN comparison
-		if( isset( $_POST['new_date'] ) && $_POST['new_date'] && isset( $_POST['old_date'] ) && $_POST['old_date'] ) {
+	// if both minimum price and maximum price are specified we will use BETWEEN comparison
+	if( isset( $_POST['new_date'] ) && $_POST['new_date'] && isset( $_POST['old_date'] ) && $_POST['old_date'] ) {
+		$args['meta_query'][] = array(
+			'key' => 'дата',
+			'value' => array( $_POST['old_date'], $_POST['new_date'] ),
+			'type' => 'date',
+			'compare' => 'between'
+		);
+	} else {
+		// if only min price is set
+		if( isset( $_POST['old_date'] ) && $_POST['old_date'] )
 			$args['meta_query'][] = array(
 				'key' => 'дата',
-				'value' => array( $_POST['old_date'], $_POST['new_date'] ),
+				'value' => $_POST['old_date'],
 				'type' => 'date',
-				'compare' => 'between'
+				'compare' => '>'
 			);
-		} else {
-			// if only min price is set
-			if( isset( $_POST['old_date'] ) && $_POST['old_date'] )
-				$args['meta_query'][] = array(
-					'key' => 'дата',
-					'value' => $_POST['old_date'],
-					'type' => 'date',
-					'compare' => '>'
-				);
  
-			// if only max price is set
-			if( isset( $_POST['new_date'] ) && $_POST['new_date'] )
-				$args['meta_query'][] = array(
-					'key' => 'дата',
-					'value' => $_POST['new_date'],
-					'type' => 'date',
-					'compare' => '<'
-				);
-			}
-		//echo " a: ";
-		
-		$args['post_type'] = "city_object";
-		//print_r ($args);
-	
-		$query = new WP_Query( $args );
-		//echo " q: ";
-		//print_r ($query);
-		
-		if ($query->have_posts()) {
-			while ($query->have_posts()) {
-				$query->the_post();
-				get_template_part('partials/posts/content', 'excerpt');
-			}
-		}
-		else {
-			get_template_part('partials/posts/content', 'none');
-		}
-		die();
+		// if only max price is set
+		if( isset( $_POST['new_date'] ) && $_POST['new_date'] )
+			$args['meta_query'][] = array(
+				'key' => 'дата',
+				'value' => $_POST['new_date'],
+				'type' => 'date',
+				'compare' => '<'
+			);
 	}
+	//echo " a: ";
+	$args['post_type'] = "city_object";
+	print_r ($args);
+ 
+	$query = new WP_Query( $args );
+	//echo " q: ";
+	//print_r ($query);
+	
+	if ($query->have_posts()) {
+		while ($query->have_posts()) {
+			$query->the_post();
+			get_template_part('partials/posts/content', 'excerpt');
+		}
+	}
+	else {
+		get_template_part('partials/posts/content', 'none');
+	}	
+	die();
 }
