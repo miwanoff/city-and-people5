b4bdbfac08fef2008b1e6db237aa2ee3c4420a2e
arhibober <alex_lvovich_iff@mail.ru>
2022-04-13 12:59:41 +0300

    The filter caption was made translatible

--- wp-content/themes/cityandpeople/sidebar.php
+++ wp-content/themes/cityandpeople/sidebar.php
@@ -6,6 +6,6 @@ if (is_active_sidebar('cityandpeople_sidebar')) {
     dynamic_sidebar('cityandpeople_sidebar');
 }
 ?>
-<h3>Фильтр</h3>
+<h3><?php _e("Filter"); ?></h3>
 <?php echo do_shortcode( '[searchandfilter fields="city_object_taxonomy" hierarchical=1]' ); ?>
 </div>
